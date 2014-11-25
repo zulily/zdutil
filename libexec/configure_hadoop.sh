1a0c570d32fa7e0a6a3e4ac8a1ebe0bb5354c576
Jason Hurt <jhurt@zulily.com>
2014-11-25 14:13:05 -0800

    increase ulimits for hadoop user

--- libexec/configure_hadoop.sh
+++ libexec/configure_hadoop.sh
@@ -104,3 +104,12 @@ if [[ -f yarn-template.xml ]]; then
         --clobber
   fi
 fi
+
+# update some ulimits for the hadoop user
+echo 'hadoop soft nproc 10000' >> /etc/security/limits.conf
+echo 'hadoop hard nproc 10000' >> /etc/security/limits.conf
+echo 'hadoop soft nofile 100000' >> /etc/security/limits.conf
+echo 'hadoop hard nofile 500000' >> /etc/security/limits.conf
+
+# make the above ulimits actually take effect
+echo 'session required pam_limits.so' >> /etc/pam.d/common-session
