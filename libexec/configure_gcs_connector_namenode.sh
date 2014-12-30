06e7a0ba4bca0553e1b54a680ccf8eec8e97c6f2
Jason Hurt <jhurt@zulily.com>
2014-12-30 11:01:45 -0800

    update maven link

--- libexec/configure_gcs_connector_namenode.sh
+++ libexec/configure_gcs_connector_namenode.sh
@@ -12,7 +12,7 @@ wget https://github.com/zulily/bigdata-interop/archive/1.3.1-z.tar.gz
 tar -zxf 1.3.1-z.tar.gz
 rm 1.3.1-z.tar.gz
 bigdata-interop-1.3.1-z/tools/generate-poms.sh
-apache-maven-3.2.3/bin/mvn -P hadoop1 package -f bigdata-interop-1.3.1-z/pom.xml-hadoop1
+apache-maven-3.2.5/bin/mvn -P hadoop1 package -f bigdata-interop-1.3.1-z/pom.xml-hadoop1
 
 #copy the jar to hadoop libs
 cp bigdata-interop-1.3.1-z/gcs/target/gcs-connector-1.3.1-SNAPSHOT-hadoop1-shaded.jar ${HADOOP_LIB_DIR}/
--- libexec/install_maven.sh
+++ libexec/install_maven.sh
@@ -5,6 +5,6 @@ echo 'Installing maven'
 set -e
 
 #install maven
-wget http://www.us.apache.org/dist/maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz
-tar -zxf apache-maven-3.2.3-bin.tar.gz
-rm apache-maven-3.2.3-bin.tar.gz
+wget http://www.us.apache.org/dist/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz
+tar -zxf apache-maven-3.2.5-bin.tar.gz
+rm apache-maven-3.2.5-bin.tar.gz
